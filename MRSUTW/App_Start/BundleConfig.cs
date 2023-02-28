0ebca2f4d57d5126d43a1b50ed44b170d086bd88
Lunca M <lunca@LAPTOP-EVSKLM5K>
2023-02-28 19:41:31 +0200

    Integrare template (bundle config)

---
+++ MRSUTW/App_Start/BundleConfig.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Web.Optimization;
+
+namespace MRSUTW
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include(
+                "~/Content/themes/bootstrap.min.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/font-awesome/css").Include(
+                "~/Content/themes/font-awesome.min.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/flaticon/css").Include(
+                "~/Content/themes/flaticon.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/owl-carousel/css").Include(
+                "~/Content/themes/owl.carousel.min.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/barfiller/css").Include(
+                "~/Content/themes/barfiller.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/magnific-popup/css").Include(
+                "~/Content/themes/magnific-popup.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/slicknav/css").Include(
+                "~/Content/themes/slicknav.min.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/style/css").Include(
+                "~/Content/themes/style.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery-3.3.1/js").Include(
+                "~/Scripts/jquery-3.3.1.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap/js").Include(
+                "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery.magnific-popup/js").Include(
+                "~/Scripts/jquery.magnific-popup.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/masonry-pkgd/js").Include(
+                "~/Scripts/masonry.pkgd.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery-barfiller/js").Include(
+                "~/Scripts/jquery.barfiller.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery-slicknav/js").Include(
+                "~/Scripts/jquery.slicknav.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/owl-carousel/js").Include(
+                "~/Scripts/owl.carousel.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/main/js").Include(
+                "~/Scripts/main.js"));
+
+        }
+    }
+
+}
--- MRSUTW/Global.asax.cs
+++ MRSUTW/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿//using eUseControl.Web;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
@@ -14,8 +16,10 @@ namespace MRSUTW
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
-           AreaRegistration.RegisterAllAreas();
-           RouteConfig.RegisterRoutes(RouteTable.Routes);
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
