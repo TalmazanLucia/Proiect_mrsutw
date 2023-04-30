84a38f5e16b1189900178556eb0b85fb97a84e8d
Lunca M <lunca@LAPTOP-EVSKLM5K>
2023-04-30 17:17:49 +0300

    Added EntityFramework

---
+++ MRSUTW.BusinessLogic/DBModel/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using MRSUTW.Domain.Entities.User;
+
+namespace MRSUTW.BusinessLogic.DBModel.Seed
+{
+     class UserContext : DbContext 
+     {
+          public UserContext() :
+               base("name=MRSUTW") //connectionstring name define in web.config
+          { 
+          }
+          public virtual DbSet<UDbTable> Users { get; set; }
+     }
+}
