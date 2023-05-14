42bff9b3dc2a62bf0cda0f8f6a32a478c51aa673
TalmazanLucia <lucydr470@gmail.com>
2023-05-14 21:29:38 +0300

    Create static pages for Admin Home and Admin Users

---
+++ MRSUTW/Controllers/AdminHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MRSUTW.Controllers
+{
+    public class AdminHomeController : Controller
+    {
+        // GET: AdminHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
