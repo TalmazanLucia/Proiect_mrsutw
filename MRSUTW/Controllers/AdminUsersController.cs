15bd8428d8ead29b70a3ed7c94fd9a7e22221c60
TalmazanLucia <lucydr470@gmail.com>
2023-05-14 23:33:35 +0300

    Conect Admin to Website and Finish Admin User

--- MRSUTW/Controllers/AdminUsersController.cs
+++ MRSUTW/Controllers/AdminUsersController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AutoMapper;
+using MRSUTW.BusinessLogic.Interfaces;
+using MRSUTW.Domain.Entities.User;
+using MRSUTW.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +12,30 @@ namespace MRSUTW.Controllers
 {
     public class AdminUsersController : Controller
     {
-        // GET: AdminUsers
-        public ActionResult Index()
-        {
-            return View();
-        }
+          private ISession _session;
+
+          public AdminUsersController()
+          {
+               var bl = new BusinessLogic.BussinesLogic();
+               _session = bl.GetSessionBL();
+          }
+
+          // GET: AdminUsers
+          public ActionResult Index()
+          {
+               var userCookie = Request.Cookies["MRSUTW"];
+               if (userCookie == null) { return RedirectToAction("Index", "Home"); }
+
+               var config = new MapperConfiguration(cfg => {
+                    cfg.CreateMap<UProfileData, User>();
+               });
+
+               IMapper mapper = config.CreateMapper();
+
+               List<UProfileData> usersProfileDataList = _session.GetUsers();
+               List<User> users = usersProfileDataList.Select(usersProfileData => mapper.Map<User>(usersProfileData)).ToList();
+
+               return View(users);
+          }
     }
 }
