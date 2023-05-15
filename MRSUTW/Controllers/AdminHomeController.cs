9e90cf03dfac796a78164d3eb57c7f4c57c5f8be
TalmazanLucia <lucydr470@gmail.com>
2023-05-15 22:55:04 +0300

    Add Users to Admin Home

--- MRSUTW/Controllers/AdminHomeController.cs
+++ MRSUTW/Controllers/AdminHomeController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AutoMapper;
+using MRSUTW.BusinessLogic.Interfaces;
+using MRSUTW.Domain.Entities.User;
+using MRSUTW.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +12,30 @@ namespace MRSUTW.Controllers
 {
     public class AdminHomeController : Controller
     {
-        // GET: AdminHome
-        public ActionResult Index()
-        {
-            return View();
-        }
+          private ISession _session;
+
+          public AdminHomeController()
+          {
+               var bl = new BusinessLogic.BussinesLogic();
+               _session = bl.GetSessionBL();
+          }
+          // GET: AdminHome
+          public ActionResult Index()
+          {
+               var userCookie = Request.Cookies["MRSUTW"];
+               if (userCookie == null) { return RedirectToAction("Index", "Home"); }
+
+               var config = new MapperConfiguration(cfg => {
+                    cfg.CreateMap<UProfileData, User>();
+               });
+
+               IMapper mapper = config.CreateMapper();
+               AdminHomeData data = new AdminHomeData();
+
+               List<UProfileData> usersProfileDataList = _session.GetUsers();
+               data.Users = usersProfileDataList.Select(usersProfileData => mapper.Map<User>(usersProfileData)).ToList();
+
+               return View(data);
+          }
     }
 }
