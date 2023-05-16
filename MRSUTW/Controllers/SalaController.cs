03fc1f4cca8a54a77f295f6e5edaf156afbb6100
Kr15tIan <95626780+Kr15tIan@users.noreply.github.com>
2023-05-16 17:33:26 +0300

    tabel-baza de date

--- MRSUTW/Controllers/SalaController.cs
+++ MRSUTW/Controllers/SalaController.cs
@@ -1,17 +1,62 @@
-﻿using System;
+﻿using System.Web.Mvc;
+using System.Data.SqlClient;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using MRSUTW.Models;
 
 namespace MRSUTW.Controllers
 {
     public class SalaController : Controller
     {
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        SqlConnection con = new SqlConnection();
+        List<ActivitiesModel> activities = new List<ActivitiesModel>();
+
+        public SalaController()
+        {
+            con.ConnectionString = MRSUTW.Properties.Resources.ConnectionString;
+        }
+
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            FetchData();
+            return View(activities);
+        }
+        private void FetchData()
+        {
+            if(activities.Count > 0)
+            {
+                activities.Clear();
+            }
+            try
+            {
+                con.Open();
+                com.Connection = con;
+                com.CommandText = "SELECT TOP (1000) [id],[Prof],[Luni],[Marti],[Miercuri],[Joi],[Vineri],[Sambata],[Duminica],[LoculDesfasurarii] FROM [MRSUTW].[dbo].[Table_Activities]";
+                dr = com.ExecuteReader();
+                while(dr.Read())
+                {
+                    activities.Add(new ActivitiesModel() {
+                        id = dr["id"].ToString(),
+                        Prof = dr["Prof"].ToString(),
+                        Luni = dr["Luni"].ToString(),
+                        Marti = dr["Marti"].ToString(),
+                        Miercuri = dr["Miercuri"].ToString(),
+                        Joi = dr["Joi"].ToString(),
+                        Vineri = dr["Vineri"].ToString(),
+                        Sambata = dr["Sambata"].ToString(),
+                        Duminica = dr["Duminica"].ToString(),
+                        LoculDesfasurarii = dr["LoculDesfasurarii"].ToString(),
+                    });
+                }
+                con.Close();
+            }
+            catch (System.Exception ex)
+            {
+
+                throw ex;
+            }
         }
     }
 }
---
+++ MRSUTW/Models/ActivitiesModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MRSUTW.Models
+{
+    public class ActivitiesModel : ApiController
+    {
+        // GET api/<controller>
+        public string id { get; set; }
+        public string Prof { get; set; }
+        public string Luni { get; set; }
+        public string Marti { get; set; }
+        public string Miercuri { get; set; }
+        public string Joi { get; set; }
+        public string Vineri { get; set; }
+        public string Sambata { get; set; }
+        public string Duminica { get; set; }
+        public string LoculDesfasurarii { get; set; }
+    }
+}
